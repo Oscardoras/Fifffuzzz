3803d0d49e077335b3d0a906bd4a044967878a7b
Oscar Buon Ryd <oscar.buon@free.fr>
2023-12-23 15:11:58 +0100

    Flowers correction

--- level/Lotus.gd
+++ level/Lotus.gd
@@ -1,7 +1,10 @@
 extends RigidBody3D
 
 
-@export var force_scale = 100
+@export var force_scale: float = 150
 
 func _ready():
-	constant_force = Vector3(randf() * force_scale, randf() * force_scale, randf() * force_scale)
+	var force_x = (randf() - 0.5) * force_scale
+	var force_y = (randf() * 0.5) * force_scale
+	var force_z = (randf() - 0.5) * force_scale
+	constant_force = Vector3(force_x, force_y, force_z)
